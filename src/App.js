49cdd95cdcedd3c567c73a69cfa6b1c205063024
Atroopa <atroopa@gmail.com>
2023-03-13 17:50:31 +0330

    props title work true

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Product from "./components/Product";
 function App() {
   return (
     <div >
-      <Product/>
-      <Product/>
-      <Product/>
+      <Product title="Apple watch RED cor i3 series A"/>
+      <Product title="Apple Watch Classic Model E720"/>
+      <Product title="Applw Watch Miror Solver+ cor i 7 series M2"/>
     </div>
   );
 }
