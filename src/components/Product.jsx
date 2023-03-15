aff545a5d56ae5344b9c1ce2d23a7a29183e3208
Atroopa <atroopa@gmail.com>
2023-03-15 15:08:28 +0330

    add product componnet into ProductList components

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,13 +9,13 @@ const Product = (props) => {
   }
 
   return (
-    <div>
+    <div key={props.key}>
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <button >
                 <img className="p-8 rounded-t-lg" src={appWatchImage} alt='apple watch' />
             </button>
             <div className="px-5 pb-5">
-                <p key={props.key}>
+                <p >
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{props.title}</h5>
                 </p>
                 <div className="flex items-center mt-2.5 mb-5">
@@ -27,7 +27,7 @@ const Product = (props) => {
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
                 </div>
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">{props.price}</span>
                     <button onClick={titleHandler} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
                 </div>
             </div>
